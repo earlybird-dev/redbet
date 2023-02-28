a712320551e514e247f8433a7b77ad4069aafc84
Ha Nguyen <31268683+earlybird-dev@users.noreply.github.com>
2023-03-01 09:32:22 +1100

    add modal

---
+++ src/components/hero/HeroModal.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import TermsAndConditions from "./TermsAndConditions";
+
+const HeroModal = (props: any) => {
+  return (
+    <>
+      <div>
+        <button
+          type="button"
+          className="btn btn-primary"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          Open Modal
+        </button>
+        <div
+          className="modal fade "
+          id="exampleModal"
+          tabIndex={-1}
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
+            <div className="modal-content text-white bg-black border-0">
+              <div className="modal-header border-0">
+                <h1
+                  className="modal-title fs-5 w-100 text-center"
+                  id="exampleModalLabel"
+                >
+                  {props.title}
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                />
+              </div>
+              <div className="modal-body border-0">
+                <img
+                  src="/images/HERO-1-1.jpg"
+                  className="d-block w-100 mb-5"
+                  alt="..."
+                ></img>
+                <div>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea
+                  nam quas in culpa dicta dignissimos quae modi dolor pariatur
+                  doloribus assumenda, magnam architecto ipsam quibusdam.
+                  Voluptas quae, eius porro provident sapiente doloremque
+                  officia hic eligendi optio minima dolore fugiat nesciunt.
+                </div>
+                <TermsAndConditions />
+              </div>
+              <div className="modal-footer border-0 d-flex flex-column align-items-center justify-content-center">
+                <button type="button" className="btn btn-primary w-100">
+                  BET NOW!
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-outline-dark text-white w-100"
+                  data-bs-dismiss="modal"
+                >
+                  CLOSE
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HeroModal;
